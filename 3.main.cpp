f4bac70f83ab4c9a21862950bc115e8215619efc
DevGupta <77541795+DevGupta-ikr@users.noreply.github.com>
2021-09-15 21:00:12 +0530

    3rd Lesson

---
+++ 3.main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "John";   // Oh Thank God for String
+    int age = 35;
+
+    cout << "There once was a man named " << name << endl;
+    cout << "He was " << age << " years old" << endl;
+
+    name = "Mike";
+    age = 25;
+    cout << "He liked the name " << name << endl;
+    cout << "But did not like being " << age << endl;
+
+    return 0;
+}
