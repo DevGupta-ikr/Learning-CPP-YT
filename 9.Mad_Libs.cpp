df4860f6d37d37e8e964ad92643ae7cced8eb56c
DevGupta <77541795+DevGupta-ikr@users.noreply.github.com>
2021-09-15 22:48:38 +0530

    9th Lesson

---
+++ 9.Mad_Libs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string color, noun, celebrity;
+
+    cout << "Enter your color: ";
+    getline(cin, color);
+    cout << "Enter a plural noun: ";
+    getline(cin, noun);
+    cout << "Enter your celebrity: ";
+    getline(cin, celebrity);
+
+    cout << "Roses are " << color << endl;
+    cout << noun << " are blue " << endl;
+    cout << "My fav is " << celebrity << endl;
+
+
+    return 0;
+}
