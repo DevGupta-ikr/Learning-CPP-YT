31c900161a1020133ab48427a8ecc94bc3d95c69
DevGupta <77541795+DevGupta-ikr@users.noreply.github.com>
2021-09-15 21:03:10 +0530

    8th Lesson

---
+++ 8.Basic_Calc.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+
+    cout << "Enter first number: " << endl;
+    cin >> num1;
+
+    cout << "Enter first number: " << endl;
+    cin >> num2;
+
+    cout << "Sum is: " << num1 + num2  << endl;
+
+
+
+
+    return 0;
+}
