63e248bae7a77b377d060b56f25e8529a2ffc942
DevGupta <77541795+DevGupta-ikr@users.noreply.github.com>
2021-09-15 23:59:32 +0530

    18th Lesson

---
+++ 18.For_Loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num[] = {1, 2, 45, 5, 90};
+
+    for(int i =0; i< 5; i++)
+    {
+        cout << num[i] << endl;
+    }
+
+    return 0;
+}
